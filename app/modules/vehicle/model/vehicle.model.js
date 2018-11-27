9d18aa1ed7cb9d6b021104fcd0d4e824bbd51dc4
Komal Bansal <aman@parangat.com>
2018-11-27 12:25:21 +0530

    model style changed

--- app/modules/vehicle/model/vehicle.model.js
+++ app/modules/vehicle/model/vehicle.model.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const vehicleSchema = new mongoose.Schema(
   {
     model: {
-      type: String,
-      unique: true
+      type: Array,
     },
     brand: {
       type: String,
